817a43e1a7ff47e45bd975a393d26607475ac0ab
kennethcollins138 <kennethcollinswork@gmail.com>
2024-12-22 00:51:37 -0800

    added some dependencies for testing/linting. Initialized project structure(subject to change)

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PortfolioOS
+
+Hello, Thank you for visiting my passion project :)  
+Currently this is a work in progress as I squeeze in time between clients.  
+Excited to work on this moving forward and always willing to answer questions about my learning journey!
--- portfolioos/eslint.config.mjs
+++ portfolioos/eslint.config.mjs
@@ -10,7 +10,44 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+      "next/core-web-vitals",
+      "next/typescript",
+      "plugin:react/recommended",
+      "plugin:react-hooks/recommended",
+      "plugin:jsx-a11y/recommended",
+      "plugin:@typescript-eslint/recommended"
+  ),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    plugins: [
+      "react-refresh",
+      "@typescript-eslint",
+      "jsx-a11y",
+    ],
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["warn"],
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "jsx-a11y/anchor-is-valid": "off",
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+    },
+  },
 ];
 
 export default eslintConfig;
